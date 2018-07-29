12bd33e4c824271859adc096506b0797345fb335
Simplejul <simplejul@protonmail.com>
2018-07-29 18:19:03 +0200

    header

--- parts/header.php
+++ parts/header.php
@@ -1,1 +1,12 @@
-put some header here
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="/../public/style.css" />
+</head>
+<body>
+    
+    <?php include "menu.php";?>
